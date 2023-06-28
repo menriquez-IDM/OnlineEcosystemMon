8407edf6b8665824749581d09f86fcd0e4ecdb5e
Minerva Enriquez <74386190+menriquez-IDM@users.noreply.github.com>
2023-06-28 11:00:07 -0700

    sample test

---
+++ Tests/GeneDriveSite/Test_GeneDrive.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Set up the Selenium WebDriver
+driver = webdriver.Chrome()  # Change this to the appropriate WebDriver for your browser
+
+# Open the website
+driver.get("https://gene-drive.bmgf.io/")
+
+# Define a list of core element locators
+element_locators = [
+    (By.CSS_SELECTOR, "h1"),  # Example: CSS selector for h1 element
+    (By.ID, "example-id"),    # Example: ID of an element
+    # Add more locators as needed for other core elements
+]
+
+# Validate the presence of each core element
+for locator in element_locators:
+    try:
+        element_present = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located(locator)
+        )
+        print("Element found:", element_present)
+    except:
+        print("Element not found:", locator)
+
+# Close the browser
+driver.quit()
