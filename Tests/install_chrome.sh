351e6690d8507f094029ae0f97ba9cb54bb163f6
Minerva Enriquez <menriquez@idmod.org>
2023-06-29 20:18:59 -0700

    adding an installer for chrome

---
+++ Tests/install_chrome.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Add the Google Chrome repository
+cat <<EOF | sudo tee /etc/yum.repos.d/google-chrome.repo
+[google-chrome]
+name=google-chrome
+baseurl=http://dl.google.com/linux/chrome/rpm/stable/\$basearch
+enabled=1
+gpgcheck=1
+gpgkey=https://dl.google.com/linux/linux_signing_key.pub
+EOF
+
+# Install Google Chrome
+yum update -y
+yum install -y google-chrome-stable
