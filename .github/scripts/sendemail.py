d82e4e66aa5b9122e10395bc99da56d332e1c0e3
Minerva Enriquez <74386190+menriquez-IDM@users.noreply.github.com>
2023-07-07 18:59:42 -0700

    fixing usr and pass not being resolved

--- .github/scripts/sendemail.py
+++ .github/scripts/sendemail.py
@@ -3,47 +3,42 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import sys, os
 
-def send_email():
 
-    sender_email = os.environ.get('SENDER_EMAIL')
-    sender_password = os.environ.get('SENDER_PASSWORD')
-    recipient_email = os.environ.get('RECIPIENT_EMAIL')
-    subject = os.environ.get('SUBJECT')
-    message = os.environ.get('MESSAGE')
-    
-    print("Sender: ", sender_email)
-    print("Recipient: ", recipient_email)
-    
-    # Set up the email message
-    msg = MIMEMultipart()
-    msg['From'] = sender_email
-    msg['To'] = recipient_email
-    msg['Subject'] = subject
+sender_email = sys.argv[1]
+sender_password = sys.argv[2]
+recipient_email = os.environ.get('RECIPIENT_EMAIL')
+subject = os.environ.get('SUBJECT')
+message = os.environ.get('MESSAGE')
 
-    # Attach the message to the email
-    msg.attach(MIMEText(message, 'plain'))
+print("Sender: ", sender_email)
+print("Recipient: ", recipient_email)
 
-    try:
-        # Connect to the SMTP server
-        server = smtplib.SMTP('smtp-relay.gmail.com', 587)  # Change this for other email providers
-        server.starttls()
+# Set up the email message
+msg = MIMEMultipart()
+msg['From'] = sender_email
+msg['To'] = recipient_email
+msg['Subject'] = subject
 
-        # Log in to the sender's email account
-        server.login(sender_email, sender_password)
+# Attach the message to the email
+msg.attach(MIMEText(message, 'plain'))
 
-        # Send the email
-        server.send_message(msg)
-        print("Email sent successfully!")
+try:
+    # Connect to the SMTP server
+    server = smtplib.SMTP('smtp-relay.gmail.com', 587)  # Change this for other email providers
+    server.starttls()
 
-    except Exception as e:
-        print(f"Error sending email: {str(e)}")
+    # Log in to the sender's email account
+    server.login(sender_email, sender_password)
 
-    finally:
-        # Disconnect from the SMTP server
-        server.quit()
+    # Send the email
+    server.send_message(msg)
+    print("Email sent successfully!")
 
- # Example usage
-#print(os.environ)
-send_email()
+except Exception as e:
+    print(f"Error sending email: {str(e)}")
+    print(os.environ)
 
+finally:
+    # Disconnect from the SMTP server
+    server.quit()
 
