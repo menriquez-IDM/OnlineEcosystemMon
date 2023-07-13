9d86f6c8f6850ba960b193dc3888f00a65e8ff87
Minerva Enriquez <menriquez@idmod.org>
2023-07-12 17:58:27 -0700

    adding os import

--- tests/UIAutoBaseClass/UIAutoBaseClass.py
+++ tests/UIAutoBaseClass/UIAutoBaseClass.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import os
 
 class ChromeTest:
     def __init__(self):
