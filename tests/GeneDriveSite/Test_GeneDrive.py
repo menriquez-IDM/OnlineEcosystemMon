b70912e9f25481040a127b24c8303b77982239d3
Minerva Enriquez <menriquez@idmod.org>
2023-07-13 00:32:50 -0700

    Cleanning up and adding screenshots

--- tests/GeneDriveSite/Test_GeneDrive.py
+++ tests/GeneDriveSite/Test_GeneDrive.py
@@ -1,20 +1,18 @@
 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from pathlib import Path
-import os, sys
+import os, sys, time
+
 os.chdir(str(Path(sys.argv[0]).parent))
 sys.path.append( str(Path('../UIAutoBaseClass').resolve().absolute()) )
 sys.path.append( str(Path('../BugReportGenerator').resolve().absolute()) )
 
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from UIAutoBaseClass import ChromeTest, ElementFinder
 from BugReportGenerator import BugReportGenerator
+from UIAutoBaseClass import ChromeTest, ElementFinder
 
-import time
-
-url = "https://gene-drive.bmgf.io/"
-    
+url = "https://gene-drive.bmgf.io/"   
 try:
     # find the button with id = close-greeting-modal and click it
     driver = ChromeTest().open_url(url) 
@@ -27,17 +25,20 @@ try:
     driver.find_element(By.ID, "elim-prob-matrices")
     print("Validated presence of element with locator: ", "elim-prob-matrices")
     time.sleep(3)
-    driver.save_screenshot("screenshot.png")
-    driver.quit()
-    
+    print("PASS:  GeneDrive test completed successfully.")
+   
 except Exception as e:
-    # try to take a screenshot
-    driver.save_screenshot("screenshot.png")
     print(e)
-    print("Exception occurred.  Generating bug report...")
+    print("Exception occurred. Generating bug report...")
     bug_report_generator = BugReportGenerator(e)
     bug_report_generator.generate_bug_report(url, title="Failure in GeneDriveSite")
     
-    driver.quit()
     
-    
+finally:
+    try:
+        driver.save_screenshot("screenshot.png")
+    except Exception as screenshot_error:
+        print("Failed to capture screenshot:", screenshot_error)
+        
+    if driver is not None:
+        driver.quit()
--- tests/LeakyVaccine/Test_LeakyVaccine.py
+++ tests/LeakyVaccine/Test_LeakyVaccine.py
@@ -32,16 +32,27 @@ try:
     driver.implicitly_wait(8)
 
     # Navigate to Parameter sweeps tab
-    driver.find_element(By.CSS_SELECTOR, "a[data-value='Parameter XXXXXXXX sweeps']").click()
+    driver.find_element(By.CSS_SELECTOR, "a[data-value='Parameter sweeps']").click()
     driver.implicitly_wait(15)
 
     # data-subplot = 'x2y'
     plotx2y = driver.find_element(By.CSS_SELECTOR, "rect[data-subplot='x2y']")
     if plotx2y:  print("PASS:  plotx2y exists.")
     else: print("plotx2y does not exist.")
+    
+    print("PASS:  LeakyVaccine test completed successfully.")
 
-    # Close the browser
-    driver.quit()
 except Exception as e:
+    print(e)
+    print("Exception occurred. Generating bug report...")
     bug_report_generator = BugReportGenerator(e)
     bug_report_generator.generate_bug_report(url, title="LeakyVaccine Page 1 and Page 2 failed to load")
+    
+finally:
+    try:
+        driver.save_screenshot("screenshot.png")
+    except Exception as screenshot_error:
+        print("Failed to capture screenshot:", screenshot_error)
+        
+    if driver is not None:
+        driver.quit()
